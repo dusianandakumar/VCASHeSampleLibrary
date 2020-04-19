e30c05baac835cc01cd9480e91ff999c281abbc4
AnandDusi <Anand.Dusi@cashe.co.in>
2020-04-20 00:09:03 +0530

    Initial project setup

---
+++ Desktop/MyLibrary/MyLibrary/MyLibrary.h
@@ -0,0 +1,19 @@
+//
+//  MyLibrary.h
+//  MyLibrary
+//
+//  Created by Anandakumar Dusi on 20/04/20.
+//  Copyright © 2020 CASHe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLibrary.
+FOUNDATION_EXPORT double MyLibraryVersionNumber;
+
+//! Project version string for MyLibrary.
+FOUNDATION_EXPORT const unsigned char MyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLibrary/PublicHeader.h>
+
+

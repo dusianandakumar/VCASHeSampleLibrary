259e167ae5d1b0d0b92f82226dbe90788526628c
AnandDusi <Anand.Dusi@cashe.co.in>
2020-04-20 00:34:08 +0530

    Initial project setup

---
+++ Desktop/VCASHeSampleLibrary/VCASHeSampleLibrary/VCASHeSampleLibrary.h
@@ -0,0 +1,19 @@
+//
+//  VCASHeSampleLibrary.h
+//  VCASHeSampleLibrary
+//
+//  Created by Anandakumar Dusi on 20/04/20.
+//  Copyright © 2020 CASHe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VCASHeSampleLibrary.
+FOUNDATION_EXPORT double VCASHeSampleLibraryVersionNumber;
+
+//! Project version string for VCASHeSampleLibrary.
+FOUNDATION_EXPORT const unsigned char VCASHeSampleLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCASHeSampleLibrary/PublicHeader.h>
+
+
